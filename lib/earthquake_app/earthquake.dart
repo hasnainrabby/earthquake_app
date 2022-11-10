80f147d167ac02a6163ff2c7cf716130de650de2
hasnainrabby <43495379+hasnainrabby@users.noreply.github.com>
2022-11-10 22:38:23 +0600

    Initial commit

---
+++ lib/earthquake_app/earthquake.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:http/http.dart';
+
+class EarthquakeApp extends StatefulWidget {
+  const EarthquakeApp({Key? key}) : super(key: key);
+
+  @override
+  State<EarthquakeApp> createState() => _EarthquakeAppState();
+}
+
+class _EarthquakeAppState extends State<EarthquakeApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:earthquake_app/earthquake_app/earthquake.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    home:EarthquakeApp() ,
+  ));
+}
+
